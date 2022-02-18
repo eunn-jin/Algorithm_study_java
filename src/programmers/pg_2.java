e93d87bfd6b3f3ca409cd60a1f0ad9a849c421d3
Eunjin <chocoding0@naver.com>
2022-02-18 18:15:25 +0900

    programmers_크레인 인형뽑기 게임

--- src/programmers/pg_2.java
+++ src/programmers/pg_2.java
@@ -4,15 +4,13 @@ import java.util.Stack;
 
 public class pg_2 {
     public static void main(String[] args) {
-        Solution s = new Solution();
         int[][] board = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
         int[] moves = {1,5,3,5,1,2,1,4};
 
-        System.out.println(s.solution( board, moves));
+        System.out.println(solution( board, moves));
     }
-}
-class Solution{
-    public int solution(int[][] board, int[] moves) {
+
+    public static int solution(int[][] board, int[] moves) {
         int answer = 0;
         int temp;
         int rows = board.length;
