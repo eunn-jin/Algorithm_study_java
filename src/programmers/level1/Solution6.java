6d070d7549822ca46a1a416452a9123a10bf6fa3
Eunjin <jincoding@naver.com>
2022-11-30 20:49:02 +0900

    프로그래머스_나머지가 1이 되는 수 찾기

---
+++ src/programmers/level1/Solution6.java
@@ -0,0 +1,17 @@
+package programmers.level1;
+
+//나머지가 1이 되는 수 찾기
+public class Solution6 {
+    public int solution(int n) {
+        int answer = 0;
+
+        for(int i=2; i<n; i++) {
+            if(n%i == 1) {
+                answer = i;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
