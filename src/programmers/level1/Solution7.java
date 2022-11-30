070a110b59904fa77cee9cb0caf3fdc0ddddb959
Eunjin <jincoding@naver.com>
2022-11-30 21:02:05 +0900

    프로그래머스_음양 더하기

---
+++ src/programmers/level1/Solution7.java
@@ -0,0 +1,18 @@
+package programmers.level1;
+
+//음양 더하기
+public class Solution7 {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+
+        for(int i=0; i<absolutes.length; i++) {
+            if(signs[i] == true ){
+                answer += absolutes[i];
+            } else {
+                answer -= absolutes[i];
+            }
+        }
+
+        return answer;
+    }
+}
